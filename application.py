b99f8de2cd8f63ca5589c2f71d3540a7305be8e2
apokolokyntosis <max.fsch@gmail.com>
2019-12-14 12:17:43 +0100

    standardize create_plots

--- application.py
+++ application.py
@@ -1,37 +1,51 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
 import numpy as np
-import matplotlib.pyplot as plt
-
 import io
+import matplotlib as plt
 from flask import Response
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 
+
 application = app = Flask(__name__)
+
+# Survey1:
+    # id  75e58d2a-5f02-4914-8181-a52047a3f76f
+    # accesskey 3bc43120cf784d489aefbc4cec9b1268
+    # ResultId 084644b9-b734-4d00-9afe-1fa82b803cb2
+# Survey 2:
+    # id e8e348cb-6a20-4b7a-8669-9bdb1b207e44
+
+
+id = "e8e348cb-6a20-4b7a-8669-9bdb1b207e44"
+result_id = "c53520d2-4bfc-4af7-ac15-42cb129eae86"
+accesskey = "3bc43120cf784d489aefbc4cec9b1268"
+url_results = "https://dxsurvey.com/api/MySurveys/getSurveyResults/{}?accessKey={}".format(id,accesskey)
 
 
 @app.route('/')
 def hello_world():
+
     return render_template('index.html')
 
 
-# GET api/MySurveys/getSurveyResults/{id}?accessKey={accessKey}&from={from}&till={till}
-# GET api/MySurveys/getSurveyResults/{75e58d2a-5f02-4914-8181-a52047a3f76f}?accessKey={3bc43120cf784d489aefbc4cec9b1268}&from={from}&till={till}
-# GET api/Survey/getSurvey?surveyId={75e58d2a-5f02-4914-8181-a52047a3f76f}
-# id  75e58d2a-5f02-4914-8181-a52047a3f76f
-# accesskey 3bc43120cf784d489aefbc4cec9b1268
-# ResultId 084644b9-b734-4d00-9afe-1fa82b803cb2
+@app.route('/results')
+def generate_results():
+    data = load_data()
 
-URL_results = "https://dxsurvey.com/api/MySurveys/getSurveyResults/75e58d2a-5f02-4914-8181-a52047a3f76f?accessKey=3bc43120cf784d489aefbc4cec9b1268"
-r = requests.get(url=URL_results)
-data = r.json()
+    return render_template("results")
 
 
-@app.route('/resultcount')
-def process_data():
+
+def result_count():
     data = load_data()
     return "ResultCount: {}".format(data["ResultCount"])
+
+
+@app.route('/resultsoverview')
+def generate_overview():
+    data = load_data()
 
 
 @app.route('/emotionsavg')
@@ -44,8 +58,9 @@ def emotionscores():
     return "Emotions average: " + str(sum(scores) / len(scores))
 
 
-@app.route('/results')
-def results():
+
+@app.route('/resultsraw')
+def results_raw():
     data = load_data()
     return data
 
@@ -65,6 +80,52 @@ def plot_png():
     return Response(output.getvalue(), mimetype="image/png")
 
 
+results_plot = {}
+
+# loads mail addresses in a list
+def get_mail():
+    data = load_data()
+    mail_list = []
+    for entry in data["Data"]:
+        if "mail_form" in entry.keys():
+            mail_list.append((entry["mail_form"]))
+    return mail_list
+
+
+# def create_plot_bin(name,category,answer):
+#     data=load_data()
+#     for answer in data["Data"]:
+#
+
+
+def create_plot(question, answer):
+    data = load_data()
+    scores = []
+    for entry in data["Data"]:
+        if "{}".format(question) in entry.keys():
+            scores.append((entry["".format(question)]))
+
+    labels =
+
+
+
+
+
+
+def create_plot_(name,category,answer):
+    data = load_data()
+    scores = []
+    for entry in data["Data"]:
+        if "{}".format(name) in entry.keys():
+            scores.append((entry["emotionsratings-widget"])
+    x, y = np.unique(np.asarray(scores), return_counts=True)
+
+    fig = create_figure(x, y)
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype="image/png")
+
+
 def create_figure(x, y):
     fig = Figure()
     axis = fig.add_subplot(1, 1, 1)
@@ -73,10 +134,9 @@ def create_figure(x, y):
 
 
 def load_data():
-    URL_results = "https://dxsurvey.com/api/MySurveys/getSurveyResults/75e58d2a-5f02-4914-8181-a52047a3f76f?accessKey=3bc43120cf784d489aefbc4cec9b1268"
-    r = requests.get(url=URL_results)
-    data = r.json()
-    return data
+    results = requests.get(url=url_results)
+    results_json = results.json()
+    return results_json
 
 
 if __name__ == '__main__':
