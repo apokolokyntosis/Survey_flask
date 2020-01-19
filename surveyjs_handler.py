849eb06a95c5a8954ab921c4a3db688538634fe5
apokolokyntosis <max.fsch@gmail.com>
2020-01-19 20:13:10 +0100

    - add surveyjs_handler.new_survey()

--- surveyjs_handler.py
+++ surveyjs_handler.py
@@ -1,16 +1,13 @@
 import requests
 import json
 
-# {Id: [A survey unique indentificator.], Json: [A new json string]}
 
 accesskey = "3bc43120cf784d489aefbc4cec9b1268"
-url_get_results = "https://dxsurvey.com/api/MySurveys/getSurveyResults/{}?accessKey={}".format(id, accesskey)
-
 with open("survey_manual.json", "r") as read_file:
     survey_json = json.load(read_file)
 
 
-def upload_survey(name):
+def create_survey(name):
     url_create_survey = "https://dxsurvey.com/api/MySurveys/create?accessKey={}&name={}&ownerId=".format(
         accesskey, name)
     response = requests.get(url=url_create_survey)
@@ -19,18 +16,16 @@ def upload_survey(name):
     return id_new
 
 
-def change_survey():
+def change_survey(post_survey_json):
     url_change_survey = "https://dxsurvey.com/api/MySurveys/changeJson?accessKey={}".format(accesskey)
     response = requests.post(url=url_change_survey, json=post_survey_json)
     print(response.request.body)
 
-id = upload_survey("test")
 
-post_survey_json = {
-  "Id": id,
-  "Text": str(survey_json),
-}
-
-change_survey()
-
-
+def new_survey(name):
+    id = create_survey(name)
+    post_survey_json = {
+        "Id": id,
+        "Text": str(survey_json),
+    }
+    change_survey(post_survey_json)
