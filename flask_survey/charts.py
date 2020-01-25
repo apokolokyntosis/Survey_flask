5639a299aac7e85345ce6fd52c75584ea2798d8b
Max Fischer <max.fsch@gmail.com>
2020-01-25 22:17:53 +0100

    - add results.html to surveylist.html - refactor charts.py - refactor parser.py

--- flask_survey/charts.py
+++ flask_survey/charts.py
@@ -8,68 +8,48 @@ from flask import Response
 from flask_survey import parser
 
 
-def create_pie(q):
+def create_charts(uid, questions):
     plt.clf()
     dirname = os.path.dirname(__file__)
     output_path = os.path.join(dirname, "static/assets/charts")
-    title = parser.parse_survey()[q]
-    q_data = parser.parse_data(q)
-    keys, counts = np.unique(q_data, return_counts=True)
-    colors = ['lightskyblue', 'lightcoral']
-    # debug
-    labels = ["Nein", "Ja"]
-    plt.pie(counts, labels=labels, autopct='%1.1f%%', colors=colors,
-            shadow=False, startangle=0)
-    plt.axis('equal')
-    fig = plt.gcf()
-    fig.suptitle(title, fontsize=16)
-    plt.savefig("{}/{}.png".format(output_path, q))
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
+    s_data = parser.load_survey(uid)
+    s_results = parser.load_results(uid)
+    title_dict = parser.get_titles(s_data)
 
+    for question in questions:
+        q_title = title_dict[question]
+        q_results = parser.get_results_for_question(s_results, question)
+        if question.endswith("pie"):
+            keys, counts = np.unique(q_results, return_counts=True)
+            colors = ['lightskyblue', 'lightcoral']
+            # debug
+            labels = ["Nein", "Ja"]
+            plt.pie(counts, labels=labels, autopct='%1.1f%%', colors=colors,
+                    shadow=False, startangle=0)
+            plt.axis('equal')
 
-# TODO: auslagern
-def create_bar(q):
-    plt.clf()
-    dirname = os.path.dirname(__file__)
-    output_path = os.path.join(dirname, "static/assets/charts")
-    title = parser.parse_survey()[q]
-    q_data = parser.parse_data(q)
-    keys, counts = np.unique(q_data, return_counts=True)
-    y_pos = np.arange(len(keys))
-    plt.bar(y_pos, counts, color="lightskyblue", alpha=0.7)
-    yint = range((min(counts)), (max(counts)) + 2)
-    plt.yticks(yint)
-    plt.xticks(y_pos, keys)
-    plt.ylabel('Anzahl')
-    plt.xlabel('Bewertung')
-    plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.2)
-    fig = plt.gcf()
-    # fig = create_figure(keys, counts)
-    fig.suptitle(title, fontsize=16)
-    fig.savefig("{}/{}.png".format(output_path, q))
-    # plt.show()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
+        elif question.endswith("cloud"):
+            keys, counts = np.unique(q_results, return_counts=True)
+            y_pos = np.arange(len(keys))
+            plt.bar(y_pos, counts, color="lightskyblue", alpha=0.7)
+            yint = range((min(counts)), (max(counts)) + 2)
+            plt.yticks(yint)
+            plt.xticks(y_pos, keys)
+            plt.ylabel('Anzahl')
+            plt.xlabel('Bewertung')
+            plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.2)
 
+        else:
+            cloud_list = " ".join(q_results)
+            wordcloud = WordCloud(width=480, height=480, margin=0, background_color="lightskyblue").generate(
+                cloud_list)
+            plt.imshow(wordcloud, interpolation='bilinear')
+            plt.axis("off")
+            plt.margins(x=0, y=0)
 
-def create_cloud(q):
-    plt.clf()
-    dirname = os.path.dirname(__file__)
-    output_path = os.path.join(dirname, "static/assets/charts")
-    title = parser.parse_survey()[q]
-    q_data = parser.parse_data(q)
-    cloud_list = " ".join(q_data)
-    wordcloud = WordCloud(width=480, height=480, margin=0, background_color="lightskyblue").generate(cloud_list)
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
-    plt.margins(x=0, y=0)
-    fig = plt.gcf()
-    fig.suptitle(title, fontsize=16)
-    fig.savefig("{}/{}.png".format(output_path, q))
-    # plt.show()
-    output = io.BytesIO()
-    FigureCanvas(fig).print_png(output)
-    return Response(output.getvalue(), mimetype="image/png")
+        fig = plt.gcf()
+        fig.suptitle(q_title, fontsize=16)
+        fig.savefig("{}/{}.png".format(output_path, question))
+        output = io.BytesIO()
+        FigureCanvas(fig).print_png(output)
+        return Response(output.getvalue(), mimetype="image/png")
--- flask_survey/parser.py
+++ flask_survey/parser.py
@@ -1,119 +1,53 @@
-# import requests
-#
-# id = "7b8ea9b3-284d-42fb-b08a-09ceb317cf2e"
-# accesskey = "3bc43120cf784d489aefbc4cec9b1268"
-# url_results = "https://dxsurvey.com/api/MySurveys/getSurveyResults/{}?accessKey={}".format(id, accesskey)
-# url_survey = "http://api.dxsurvey.com/api/Survey/getSurvey?surveyId={}".format(id)
-#
-#
-# def get_questions():
-#     data = load_data()
-#     questions = []
-#     first_dict = data["Data"][0]
-#     for key in first_dict:
-#         if key.startswith("question"):
-#             questions.append(str(key))
-#     return questions
-#
-#
-# # parses values for a specific question into a list
-# def parse_data(q):
-#     data = load_data()
-#     q_results = []
-#     for entry in data["Data"]:
-#         if "{}".format(q) in entry.keys():
-#             q_results.append(str(entry[q]))
-#     return q_results
-#
-#
-# # parse survey_json for question titles -> returns dict with question
-# # as key, and title as value
-# def parse_survey():
-#     survey = load_survey()
-#     title_dict = {"question1": "Wie hat Ihnen die heutige Veranstaltung gefallen?",
-#                   "question2pie": "Fühlen Sie sich ausreichend repräsentiert?",
-#                   "question3pie": "Fühlen Sie sich ausreichend informiert?",
-#                   "question4cloud": "Welchem Thema wurde ihrer Meinung\n nach noch nicht genug Beachtung geschenkt?"
-#                   }
-#     # for page in survey["pages"]:
-#     #     title_dict[page[0]["elements"][0]["name"]] = page[0]["elements"][0]["title"]["de"]
-#     #     print(title_dict)
-#     return title_dict
-#
-#
-# def load_data():
-#     results = requests.get(url=url_results)
-#     results_json = results.json()
-#     return results_json
-#
-#
-# def load_survey():
-#     survey = requests.get(url=url_survey)
-#     survey_json = survey.json()
-#     return survey_json
-#
-#
-# def result_count():
-#     data = load_data()
-#     return data["ResultCount"]
-
-
-
 import requests
 
-id = "7b8ea9b3-284d-42fb-b08a-09ceb317cf2e"
 accesskey = "3bc43120cf784d489aefbc4cec9b1268"
-url_survey = "http://api.dxsurvey.com/api/Survey/getSurvey?surveyId={}".format(id)
 
 
 def get_questions(uid):
-    data = load_data(uid)
+    dict = get_titles(load_survey(uid))
     questions = []
-    first_dict = data["Data"][0]
-    for key in first_dict:
-        if key.startswith("question"):
+    for key in dict:
             questions.append(str(key))
     return questions
 
 
-# parses values for a specific question into a list
-def parse_data(q):
-    data = load_data()
+# parses results for a specific question into a list
+def get_results_for_question(s_results, question):
     q_results = []
-    for entry in data["Data"]:
-        if "{}".format(q) in entry.keys():
-            q_results.append(str(entry[q]))
+    for entry in s_results["Data"]:
+        if "{}".format(question) in entry.keys():
+            q_results.append(str(entry[question]))
     return q_results
 
 
-# parse survey_json for question titles -> returns dict with question
-# as key, and title as value
-def parse_survey():
-    survey = load_survey()
-    title_dict = {"question1": "Wie hat Ihnen die heutige Veranstaltung gefallen?",
-                  "question2pie": "Fühlen Sie sich ausreichend repräsentiert?",
-                  "question3pie": "Fühlen Sie sich ausreichend informiert?",
-                  "question4cloud": "Welchem Thema wurde ihrer Meinung\n nach noch nicht genug Beachtung geschenkt?"
-                  }
-    # for page in survey["pages"]:
-    #     title_dict[page[0]["elements"][0]["name"]] = page[0]["elements"][0]["title"]["de"]
-    #     print(title_dict)
+# creates a dict consisting of q_name : q_title
+def get_titles(s_data):
+    s_data_clean = s_data["pages"]
+    title_dict = {}
+    for page in s_data_clean:
+        title_dict[page["elements"][0]["name"]] = page["elements"][0]["title"]
     return title_dict
 
 
-def load_data(uid):
+def load_results(uid):
     url_results = "https://dxsurvey.com/api/MySurveys/getSurveyResults/{}?accessKey={}".format(uid, accesskey)
     results = requests.get(url=url_results)
     results_json = results.json()
     return results_json
 
 
-def load_survey():
+def load_survey(uid):
+    url_survey = "http://api.dxsurvey.com/api/Survey/getSurvey?surveyId={}".format(uid)
     survey = requests.get(url=url_survey)
     survey_json = survey.json()
     return survey_json
 
 
 def result_count():
-    data = load_data()
+    data = load_results()
     return data["ResultCount"]
+
+# print(load_survey("a60d46ab-75c2-42fe-81cb-acc0653855c9"))
+# print(load_results("a60d46ab-75c2-42fe-81cb-acc0653855c9"))
+# print(get_questions("a60d46ab-75c2-42fe-81cb-acc0653855c9"))
+# print(get_results_for_question(load_results("a60d46ab-75c2-42fe-81cb-acc0653855c9"), "question1"))
